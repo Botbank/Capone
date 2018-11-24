ef1db1814e3b7751c115dd2796b0e5647cf864d3
Vuk Djukic <vukdukic@gamil.com>
2018-11-24 08:23:24 -0800

    commiting config.js

---
+++ config.js
@@ -0,0 +1,52 @@
+/** 
+module.exports = {
+    FB_PAGE_TOKEN:'EAAD6GpGrecsBANjhbUigmFVevURs9m9WVyRVTcvRilUiwazR7uFZBwaafVq3Wvcbr9zInLieH0j6WnQFcUDWvcofEixLh69bO2hbZC4UJRspXlrRKmv6kKEwkZCPZAg6f19oLRmZAZAZCRHVanOduD5QQMAdFXK7AJGS3Shg84BAQZDZD',
+    FB_APP_ID: '274992020027851',
+    FB_VERIFY_TOKEN:'beba2019$$',
+    FB_APP_SECRET:'cb291e67c2be38e1eafab5f931f8f7c8',
+    SERVER_URL:'https://bot-bank.herokuapp.com/',
+    GOOGLE_PROJECT_ID:'banking-123d5',
+    DF_LANGUAGE_CODE:'en-US',
+    GOOGLE_CLIENT_EMAIL:'dialogflow-client@banking-123d5.iam.gserviceaccount.com',
+    GOOGLE_PRIVATE_KEY:'-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC4xBK6ot7MpHIV\nGonrNe+4/n5ma2WYRXXlNiLTamgE8A0KBlYDJPcpTb3y0Elzb6iwceUAqepTWBun\nTeeT9rayJr1OPBlpnV0Fj8K1JbfXv9jwAMRcI1Xxms0ahIZVpFUVgsziKPtukDks\niXS9LDgYvK37R4niKqiKwsdr31rLx3KtApeaTTcqsGCOIt9EQ9WnX3uAusyp+Chn\nROQlYZzFjvzU6VTBlSaqPZV8a1q+8MvlGeD/0J9vql1Zbc4tCEAstIA0YLwPgaUd\nQGRW4EIAJU0yjBgwIvAy7rFi9PX300O0qY0nCEVH5nruimLnzgWoLpA4IhlReBwk\no/1IZDVTAgMBAAECggEAAT8BSVTz37l0M4Q8TePNPBgrThGHW+GMDAGzdm2PSgk8\nbUbhqZ/EMvkdQNf6xtaH8rix7LE4rVs4xIYX+718OfNc73wkR7wR9omjrIkWllPq\n6KN4r45lc/2xSG6AYW98S2rYauYP3A0I9a3vpZ74ueH74mf3HBlhjZ4GrkmRWTJ6\nNjbi8YQm81JxPBQz6AvIM7DkkhAL53heTj1OaAjF+XnLUhljlCadBggO6y/zd9Is\ngQQzjaJkjIozaZQUa4yskiKeDV3uZhNJ/6O2kNnqmLM1fvIV3Ou040IZWkbejC0m\njfXh3RlzOvNo8TeRjZC/DxGyu/MU4zTy9YRblPOdwQKBgQDyB72R3kmTQuMbfAFA\nCccI38jdwjUA1LD/17JwFDGgk+khxn46I1w86J+6A0/9dE6tMDMothUaD/qcEvq7\niSIb3gBmaNMmv9/olKG2o2CTb+9DQe7wW2JcGVe0N+fKGX5G3+Z0lhrJ5gyAc7wK\n9JMPeXuNdYAwV7ibqtBwQMjkwwKBgQDDbjCo1N0kygkuwoeRLxWPsPH53Ehr998I\n7siF0xxJqRgSuEmyoZYPm+wTgwdK477kGUVgWNKVDQIG58HXxyy7ldWVv/qbGV6C\nE8qT4978+bxL4KOjMavWuggnz2ldEqIs1tNqPewh9JgDYi8zYkK1a6eKfmUwoKXr\n7zexVtckMQKBgChSDczNQWidDgS4JeUrO9NIpMf84KOgy1Pz9TAizRm5Sm9sB/9b\ngLRM9L8iaiMJS0oi64Luwpky7EcwCkFP2TTXSS3GbNbZxE2U5VPt59roHO4Rf+Le\n9oXtWuUVIzJSCO15w4TMtO3Ju29y2HLix9UeIe0xFAf5tTGJxWvN/ynNAoGAXp5Q\n7MzyrMcsnvkMDdzQAZvX6/MdFTkTQQ3rA+/sODWoNBgJhnT5ZGFHcUL1GusdFu6t\nSjJ28jc0DgfTMBd8ru2iVTh3qvQjb7MJV4TyB2UaJ1qBn5eC7RlwrWWcS7Wj0zXH\nch683su/dZPp6mTei1ut6D293EdLTemp+zBiRWECgYEAnSqTdW7zoZcpIqJSAw2M\nQnECjBoH0rdVUSoZyCnf38r3DAbPnF1sv6mpJNOKjIodZ4y3JwRBsczZhB1f8T+A\nhponjZWBDVFIsIBi4oYI/mzheuG8IO6uGeXfB1NQcjYdp0cBx0Xaln89/78clzRq\nWYsY1mL5NFRgnOsSFMSY8X8=\n-----END PRIVATE KEY-----\n',
+    SENGRID_API_KEY: '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',
+    EMAIL_FROM: 'vukdukic@gmail.com',
+    EMAIL_TO: 'vukdukic@gmail.com',
+    WEATHER_API_KEY: 'b6907d289e10d714a6e88b30761fae22',
+    PG_CONFIG: {
+        user: 'nchmqsexcrdcue',
+        database: 'd2uob81pn1k7nh',
+        password: 'aa27ae94d12aeb9773f1225c6fb85e7ada7723ac4737abb34d4c43cc8605a188',
+        host: 'ec2-54-235-240-126.compute-1.amazonaws.com',
+        port: 5432,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    }
+};
+*/
+module.exports = {
+    FB_PAGE_TOKEN: process.env.FB_PAGE_TOKEN,
+    FB_APP_ID: process.env.FB_APP_ID,
+    FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN,
+    FB_APP_SECRET: process.env.FB_APP_SECRET,
+    SERVER_URL: process.env.SERVER_URL,
+    GOOGLE_PROJECT_ID: process.env.GOOGLE_PROJECT_ID,
+    DF_LANGUAGE_CODE: process.env.DF_LANGUAGE_CODE,
+    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+    SENGRID_API_KEY: process.env.SENGRID_API_KEY,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    EMAIL_TO: process.env.EMAIL_TO,
+    WEATHER_API_KEY: process.env.WEATHER_API_KEY,
+//    ADMIN_ID: process.env.ADMIN_ID,
+    PG_CONFIG: {
+        user: process.env.PG_CONFIG_USER,
+        database: process.env.PG_CONFIG_DATABASE,
+        password: process.env.PG_CONFIG_PASSWORD,
+        host: process.env.PG_CONFIG_HOST,
+        port: 5432,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    },
+    FB_PAGE_INBOX_ID: process.env.FB_PAGE_INBOX_ID
+};
